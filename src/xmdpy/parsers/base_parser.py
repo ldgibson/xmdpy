15972e5dccc7a384225e3a38445b5e1ca832ac30
Luke D Gibson <ldgibson819@gmail.com>
2025-10-07 09:57:52 -0400

    basic layout of protocol and classes

--- src/xmdpy/parsers/base_parser.py
+++ src/xmdpy/parsers/base_parser.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from collections.abc import Callable, Container, Generator, Sequence
-from typing import BinaryIO
+from typing import Any, BinaryIO
 
-from xmdpy.types import IntArray, PathLike
+import numpy as np
+
+from xmdpy.types import FloatLike, IntArray, PathLike
 
 
 def frame_generator(
@@ -63,3 +66,42 @@ def count_lines(filename: PathLike) -> int:
     f = open(filename, "rb", buffering=0)
     f_gen = _make_gen(f.read)
     return sum(buffer.count(b"\n") for buffer in f_gen)
+
+
+TrajSlice = np.ndarray[tuple[int, ...], np.dtype[FloatLike]]
+type TrajData = dict[str, Any]
+
+
+class TrajectoryParser(ABC):
+    @abstractmethod
+    def get_dimensions(self) -> tuple[int, int, int]:
+        """Gets the number of frames, atoms, and spatial dimensions in the trajectory"""
+
+    @abstractmethod
+    def get(
+        self, properties: list[str], key: tuple[IntArray, IntArray, IntArray]
+    ) -> TrajData:
+        """Gets property data from a slice of the trajectory"""
+
+    @abstractmethod
+    def get_atoms(self) -> list[str]:
+        """Gets the list of atoms ordered as they appear in the trajectory"""
+
+    @abstractmethod
+    def get_positions(self, key: tuple[IntArray, IntArray, IntArray]) -> TrajSlice:
+        """Gets the positions for a slice of the trajectory"""
+
+    def get_cell(self, key: tuple[IntArray, IntArray, IntArray]) -> TrajSlice | None:
+        """Gets the cell parameters for a slice of the trajectory if they exist"""
+        return None
+
+
+# filename = ".xyz"
+# traj = Trajectory(filename, dt=1)
+
+# data_vars = traj.get_data_vars()
+# coords = traj.get_coordinates()
+# attrs = traj.get_attributes()
+
+# data = data_vars["xyz"][1]
+# x = data[(0, 1)]
