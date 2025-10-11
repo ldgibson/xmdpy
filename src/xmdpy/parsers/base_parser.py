406a9efb57bb9eeab180b197bc1e97df31f509e7
Luke D Gibson <ldgibson819@gmail.com>
2025-10-11 13:40:46 -0400

    cleaning up unused code or comments

--- src/xmdpy/parsers/base_parser.py
+++ src/xmdpy/parsers/base_parser.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
 from collections.abc import Callable, Container, Generator, Sequence
-from typing import Any, BinaryIO
+from typing import BinaryIO
 
-import numpy as np
-
-from xmdpy.types import FloatLike, IntArray, PathLike
+from xmdpy.types import Int1DArray, PathLike
 
 
 def frame_generator(
     f: BinaryIO,
-    frames: Sequence[int] | IntArray,
+    frames: Sequence[int] | Int1DArray,
     lines_per_frame: int,
     skip_lines_in_frame: int | Container[int] = 0,
     usecol: slice[int | None] | None = None,
@@ -66,42 +63,3 @@ def count_lines(filename: PathLike) -> int:
     f = open(filename, "rb", buffering=0)
     f_gen = _make_gen(f.read)
     return sum(buffer.count(b"\n") for buffer in f_gen)
-
-
-TrajSlice = np.ndarray[tuple[int, ...], np.dtype[FloatLike]]
-type TrajData = dict[str, Any]
-
-
-class TrajectoryParser(ABC):
-    @abstractmethod
-    def get_dimensions(self) -> tuple[int, int, int]:
-        """Gets the number of frames, atoms, and spatial dimensions in the trajectory"""
-
-    @abstractmethod
-    def get(
-        self, properties: list[str], key: tuple[IntArray, IntArray, IntArray]
-    ) -> TrajData:
-        """Gets property data from a slice of the trajectory"""
-
-    @abstractmethod
-    def get_atoms(self) -> list[str]:
-        """Gets the list of atoms ordered as they appear in the trajectory"""
-
-    @abstractmethod
-    def get_positions(self, key: tuple[IntArray, IntArray, IntArray]) -> TrajSlice:
-        """Gets the positions for a slice of the trajectory"""
-
-    def get_cell(self, key: tuple[IntArray, IntArray, IntArray]) -> TrajSlice | None:
-        """Gets the cell parameters for a slice of the trajectory if they exist"""
-        return None
-
-
-# filename = ".xyz"
-# traj = Trajectory(filename, dt=1)
-
-# data_vars = traj.get_data_vars()
-# coords = traj.get_coordinates()
-# attrs = traj.get_attributes()
-
-# data = data_vars["xyz"][1]
-# x = data[(0, 1)]
