491bb6b294e0a6e2159a5ddf50496782f38127ee
Luke D Gibson <ldgibson819@gmail.com>
2026-01-20 09:20:10 -0500

    moving functions into OnDiskXYZTrajectory class

--- src/xmdpy/backends/xyz.py
+++ src/xmdpy/backends/xyz.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
-from functools import partial
 from typing import Any
 
 import numpy as np
@@ -10,64 +11,6 @@ from .names import DATA_VAR_DIMS, DEFAULT_COORDS, Coord, DataVar
 from .on_disk_array import OnDiskArray
 from .parsing_utils import count_lines, frame_generator
 from .trajectory_formats import TrajectoryFormat
-
-
-def get_xyz_dims_and_details(
-    filename: PathLike,
-) -> tuple[int, list[str]]:
-    n_lines = count_lines(filename)
-
-    atoms = []
-
-    with open(filename, "rb") as f:
-        n_atoms = int(f.readline().strip())
-
-        _ = f.readline()
-
-        for _ in range(n_atoms):
-            line = f.readline()
-            fields = line.split()
-            atoms.append(fields[0].decode())
-
-    n_frames = int(n_lines / (n_atoms + 2))
-
-    return n_frames, atoms
-
-
-def read_xyz_frames(
-    frames: Int1DArray,
-    atoms: Int1DArray,
-    xyz_dim: Int1DArray,
-    filename: PathLike,
-    total_atoms: int,
-    dtype: SingleDType = "float64",
-) -> TrajArray:
-    offset = 2
-    lines_per_frame = total_atoms + offset
-
-    for dim in (frames, atoms, xyz_dim):
-        if not isinstance(dim, np.ndarray):
-            raise TypeError(f"invalid index type: {type(dim)}")
-
-    skipped_lines = set(range(offset)).union(
-        {atom_id + offset for atom_id in range(total_atoms) if atom_id not in atoms}
-    )
-
-    positions = np.zeros((len(frames), len(atoms), 3), dtype=dtype)
-
-    with open(filename, "rb") as file_handle:
-        for i, coords in enumerate(
-            frame_generator(
-                file_handle,
-                frames,
-                lines_per_frame,
-                skip_lines_in_frame=skipped_lines,
-                usecol=slice(1, 4),
-            )
-        ):
-            positions[i] = coords
-
-    return positions[:, :, xyz_dim]
 
 
 @dataclass
@@ -81,18 +24,68 @@ class OnDiskXYZTrajectory:
     atoms: list[str] = field(init=False)
 
     def __post_init__(self) -> None:
-        self.n_frames, self.atoms = get_xyz_dims_and_details(self.filename)
+        self.n_frames, self.atoms = self.get_xyz_dims_and_details()
         self.n_atoms = len(self.atoms)
+
+    def get_xyz_dims_and_details(self) -> tuple[int, list[str]]:
+        n_lines = count_lines(self.filename)
+
+        atoms = []
+
+        with open(self.filename, "rb") as f:
+            n_atoms = int(f.readline().strip())
+
+            _ = f.readline()
+
+            for _ in range(n_atoms):
+                line = f.readline()
+                fields = line.split()
+                atoms.append(fields[0].decode())
+
+        n_frames = int(n_lines / (n_atoms + 2))
+
+        return n_frames, atoms
+
+    def get_positions(
+        self,
+        frames: Int1DArray,
+        atoms: Int1DArray,
+        xyz_dim: Int1DArray,
+    ) -> TrajArray:
+        offset = 2
+        lines_per_frame = self.n_atoms + offset
+
+        for dim in (frames, atoms, xyz_dim):
+            if not isinstance(dim, np.ndarray):
+                raise TypeError(f"invalid index type: {type(dim)}")
+
+        skipped_lines = set(range(offset)).union(
+            {
+                atom_id + offset
+                for atom_id in range(self.n_atoms)
+                if atom_id not in atoms
+            }
+        )
+
+        positions = np.zeros((len(frames), len(atoms), 3), dtype=self.dtype)
+
+        with open(self.filename, "rb") as file_handle:
+            for i, coords in enumerate(
+                frame_generator(
+                    file_handle,
+                    frames,
+                    lines_per_frame,
+                    skip_lines_in_frame=skipped_lines,
+                    usecol=slice(1, 4),
+                )
+            ):
+                positions[i] = coords
+
+        return positions[:, :, xyz_dim]
 
     @property
     def positions(self) -> OnDiskArray:
-        parser_fn = partial(
-            read_xyz_frames,
-            filename=self.filename,
-            total_atoms=self.n_atoms,
-            dtype=self.dtype,
-        )
-        return OnDiskArray(parser_fn, (self.n_frames, self.n_atoms, 3))
+        return OnDiskArray(self.get_positions, (self.n_frames, self.n_atoms, 3))
 
     def get_data_vars(self) -> tuple[tuple[str, tuple[str, ...], OnDiskArray], ...]:
         return ((DataVar.POSITIONS, DATA_VAR_DIMS[DataVar.POSITIONS], self.positions),)
--- tests/backends/test_xyz.py
+++ tests/backends/test_xyz.py
@@ -3,7 +3,9 @@ import os
 import numpy as np
 import pytest
 
-from xmdpy.backends.xyz import get_xyz_dims_and_details, read_xyz_frames
+from xmdpy.backends.xyz import OnDiskXYZTrajectory
+
+# from xmdpy.backends.xyz import get_xyz_dims_and_details, read_xyz_frames
 
 
 @pytest.fixture
@@ -37,14 +39,21 @@ def filename() -> str:
 
 
 @pytest.fixture
+def mock_on_disk_traj() -> OnDiskXYZTrajectory:
+    return OnDiskXYZTrajectory(filename="./tests/data/mock_traj.xyz", dt=1)
+
+
+@pytest.fixture
 def expected() -> np.ndarray:
     return np.stack(
         [np.array([[0.0, 0.0, i], [0.9, 0.7, i], [-0.9, 0.7, i]]) for i in range(10)]
     )
 
 
-def test_get_xyz_dims_and_details(fake_filesystem, filename: str) -> None:
-    n_frames, atoms = get_xyz_dims_and_details(filename)
+def test_get_xyz_dims_and_details(
+    fake_filesystem, mock_on_disk_traj: OnDiskXYZTrajectory
+) -> None:
+    n_frames, atoms = mock_on_disk_traj.get_xyz_dims_and_details()
     assert n_frames == 10
     assert atoms == ["O", "H", "H"]
 
@@ -56,19 +65,16 @@ def test_get_xyz_dims_and_details(fake_filesystem, filename: str) -> None:
 @pytest.mark.parametrize("xyz_dim", [np.arange(3), np.array([0, 1]), np.array([2])])
 def test_read_xyz_frames_slice_frames_atoms_xyzdim(
     fake_filesystem,
-    filename: str,
+    mock_on_disk_traj: OnDiskXYZTrajectory,
     frames: np.ndarray,
     atoms: np.ndarray,
     xyz_dim: np.ndarray,
     expected: np.ndarray,
 ) -> None:
-    result = read_xyz_frames(
+    result = mock_on_disk_traj.get_positions(
         frames,
         atoms,
         xyz_dim,
-        filename,
-        total_atoms=3,
-        dtype=np.float64,
     )
 
     np.testing.assert_allclose(result, expected[frames][:, atoms][:, :, xyz_dim])
@@ -76,56 +82,47 @@ def test_read_xyz_frames_slice_frames_atoms_xyzdim(
 
 def test_read_xyz_frames_frames_not_array_raises_error(
     fake_filesystem,
-    filename: str,
+    mock_on_disk_traj: OnDiskXYZTrajectory,
 ) -> None:
     frames = np.arange(10).tolist()
     atoms = np.arange(3)
     xyz_dim = np.arange(3)
 
     with pytest.raises(TypeError):
-        _ = read_xyz_frames(
+        _ = mock_on_disk_traj.get_positions(
             frames,  # type: ignore
             atoms,
             xyz_dim,
-            filename,
-            total_atoms=3,
-            dtype=np.float64,
         )
 
 
 def test_read_xyz_frames_atoms_not_array_raises_error(
     fake_filesystem,
-    filename: str,
+    mock_on_disk_traj: OnDiskXYZTrajectory,
 ) -> None:
     frames = np.arange(10)
     atoms = np.arange(3).tolist()
     xyz_dim = np.arange(3)
 
     with pytest.raises(TypeError):
-        _ = read_xyz_frames(
+        _ = mock_on_disk_traj.get_positions(
             frames,
             atoms,  # type: ignore
             xyz_dim,
-            filename,
-            total_atoms=3,
-            dtype=np.float64,
         )
 
 
 def test_read_xyz_frames_xyzdim_not_array_raises_error(
     fake_filesystem,
-    filename: str,
+    mock_on_disk_traj: OnDiskXYZTrajectory,
 ) -> None:
     frames = np.arange(10)
     atoms = np.arange(3)
     xyz_dim = np.arange(3).tolist()
 
     with pytest.raises(TypeError):
-        _ = read_xyz_frames(
+        _ = mock_on_disk_traj.get_positions(
             frames,
             atoms,
             xyz_dim,  # type: ignore
-            filename,
-            total_atoms=3,
-            dtype=np.float64,
         )
