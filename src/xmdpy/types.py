d0bd8eb358e063b63012cdecf2943c6d377b2cb4
Luke Gibson <ldgibson819@gmail.com>
2026-03-26 10:45:44 -0700

    updating type definitions

--- src/xmdpy/types.py
+++ src/xmdpy/types.py
@@ -7,13 +7,13 @@ type SingleDType = np.dtype | type | str
 type FloatLike = np.floating | np.integer
 type AnyTrajData = FloatLike | np.str_
 
-TrajArray = np.ndarray[tuple[int, int, int], np.dtype[FloatLike]]
-CellArray = np.ndarray[tuple[int, int, int], np.dtype[FloatLike]]
-CellArray3x3 = np.ndarray[tuple[int, int], np.dtype[FloatLike]]
+type TrajArray = np.ndarray[tuple[int, int, int], np.dtype[FloatLike]]
+type CellArray = np.ndarray[tuple[int, int, int], np.dtype[FloatLike]]
+type CellArray3x3 = np.ndarray[tuple[int, int], np.dtype[FloatLike]]
 
-Int1DArray = np.ndarray[tuple[int], np.dtype[np.integer]]
+type Int1DArray = np.ndarray[tuple[int], np.dtype[np.integer]]
 
 type _TrajNDArray[Shape: tuple[int, ...]] = np.ndarray[Shape, np.dtype[AnyTrajData]]
-TrajNDArray = _TrajNDArray[tuple[int, ...]]
+type TrajNDArray = _TrajNDArray[tuple[int, ...]]
 
 type PathLike = str | bytes | os.PathLike[Any]
